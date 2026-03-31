2dea920e7d2679ecac967df7cc43022b57bfaee7
bendin99 <info@h48va.cz>
2026-03-31 23:27:17 +0200

    Update sw.js

--- fltd/sw.js
+++ fltd/sw.js
@@ -1,3 +1,37 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = "fdp-tool-v4";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
   self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
 });
